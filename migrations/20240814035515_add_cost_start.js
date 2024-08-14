6bd0096988052b5053e5844ace5250f6e4bb8ec7
thomasqm <thomas@engineeringvirtue.net>
2024-08-13 20:56:05 -0700

    goddamnit!

--- migrations/20240814035515_add_cost_start.js
+++ migrations/20240814035515_add_cost_start.js
@@ -5,8 +5,8 @@
 exports.up = function(knex) {
   return knex.schema.alterTable("user", tb=>{
 		//-_-
-		if (!knex.schema.hasColumn("user", "cost"))
-			tb.double("cost").defaultTo(0).notNullable();
+		if (!knex.schema.hasColumn("user", "costStart"))
+			tb.timestamp("costStart");
 	});
 };
 
@@ -15,9 +15,5 @@ exports.up = function(knex) {
  * @returns { Promise<void> }
  */
 exports.down = function(knex) {
-  return knex.schema.alterTable("user", tb=>{
-		//not supported by sqlite...
-		// tb.dropColumn("cost");
-		// tb.dropColumn("costStart");
-	});
+  
 };
